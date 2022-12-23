7ab68efafcc83b0d5cfbbfa9775bfe74dc095b3a
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2022-12-23 14:09:00 +0530

    About section pic size fix

--- lib/sections/about_section.tsx
+++ lib/sections/about_section.tsx
@@ -17,7 +17,7 @@ const AboutSection = (props: Props) => {
         {/* <div className="dash-title text-lg">About</div> */}
         <div className="self-center font-semibold text-2xl">WHO AM I ?</div>
         <div className="flex flex-row items-center mt-5">
-          <div className="mr-10">
+          <div className="mr-10 w-3/5">
             <Image
               {...props.data.image}
               alt="My Pic 2"
@@ -27,7 +27,7 @@ const AboutSection = (props: Props) => {
               blurDataURL={props.data.blurUrl}
             />
           </div>
-          <div className="flex flex-col py-6">
+          <div className="flex flex-col py-6 grow">
             <div className="text-lg text-justify">{props.data.message}</div>
             <div className="grid grid-rows-2 grid-cols-2 gap-4 mt-5">
               {props.data.infoCards.map((info) => (
