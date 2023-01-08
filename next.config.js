2d8452ece952856950952f72786902c3209e1a44
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-01-08 19:03:41 +0530

    next config domains update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  // experimental: {
+  //   runtime: "experimental-edge",
+  // },
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "firebasestorage.googleapis.com"],
+    domains: [
+      "localhost",
+      "firebasestorage.googleapis.com",
+      "https://nirmalcode-developer-portfolio-v3-next.pages.dev/",
+    ],
   },
 };
